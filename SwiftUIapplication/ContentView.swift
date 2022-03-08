338aa46d7eb221c4720c01bc0f7dc1b36ed668f2
Érika Moriya <erika.moriya@thoughtworks.com>
2022-03-08 10:23:57 -0300

    initial commit

---
+++ SwiftUIapplication/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  SwiftUIapplication
+//
+//  Created by LA29345 on 08/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+//                .position(y:482)
+                .padding(.leading, 100)
+
+
+
+
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIapplication/SwiftUIapplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIapplicationApp.swift
+//  SwiftUIapplication
+//
+//  Created by LA29345 on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIapplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
