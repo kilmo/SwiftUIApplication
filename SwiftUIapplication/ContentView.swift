4316f483ae7877652aacec4073f0edc143246add
Érika Moriya <erika.moriya@thoughtworks.com>
2022-04-14 16:12:46 -0300

    adding list

--- SwiftUIapplication/ContentView.swift
+++ SwiftUIapplication/ContentView.swift
@@ -8,16 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    var rows: [String] = ["Hello World", "Try Again!", "Sei lá??"]
+
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-//                .position(y:482)
-                .padding(.leading, 100)
-
-
-
-
-
+        List {
+            ForEach(0..<rows.count) { index in
+                Row(textToShow: rows[index])
+            }
         }
     }
 }
@@ -25,5 +22,15 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Row: View {
+    var textToShow: String
+
+    var body: some View {
+        Text(textToShow)
+//            .position(y:482)
+//            .padding(.leading, 100)
     }
 }
